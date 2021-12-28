c2025041574c9dd7ed4e29b5bdb5a1530b77cbbf
Host Laravel <testinglaravel18@gmail.com>
2021-12-28 22:24:34 +0700

    commit-kbm

--- routes/web.php
+++ routes/web.php
@@ -21,107 +21,52 @@ use App\Http\Controllers\JadwalMapelController;
 |
 */
 
-// Route::get('/', function () {
-    
-// });
 
-// Route::get('/jadwal-pelajaran/kelas1smp', function () {
-//     return view('jadwal-pelajaran.kelas1smp');
-// });
+Route::get('/daftarmapel/1smp',[JadwalMapelController::class,'mapel'])->name('mapel')->middleware('auth');
 
-// Route::get('/register', function () {
-//     return view('user.register');
-// });
-
-// Route::get('/login', function () {
-//     return view('user.login');
-// });
-
-// Route::get('/alpha/1smp', function () {
-//     return view('daftar-alpha.alpha1smp');
-// });
-
-// Route::get('/kirim-tugas/1smp', function () {
-//     return view('pesan-murid.kirim-tugas');
-// });
-
-// Route::get('/profile', function () {
-//     return view('user.profile');
-// });
-
-// Route::get('/profile/guru', function () {
-//     return view('user.profile-guru');
-// });
-
-Route::get('/input/data', function () {
-    return view('input-data.input-data');
-});
-
-// Route::get('/home-page', function () {
-//     return view('halaman');
-// });
-
-// Route::get('/halaman/demo', function () {
-//     return view('halaman-demo');
-// });
-
-// Route::get('/tambah/jadwal', function () {
-//     return view('jadwal-pelajaran.tambah-jadwal');
-// });
-
-// Route::get('/mapel', function () {
-//     return view('jadwal-pelajaran.mapel');
-// });
-
-Route::get('/tambah/alpha', function () {
-    return view('daftar-alpha.tambah-alpha');
-});
-
-Route::get('/daftarmapel/1smp',[JadwalMapelController::class,'mapel'])->name('mapel');
-
-Route::get('/bang/{id}/{mapel_id}', [JadwalMapelController::class,'jadwalmapel']);
+Route::get('/bang/{id}/{mapel_id}', [JadwalMapelController::class,'jadwalmapel'])->middleware('auth');
 
 Route::get('/kirim-tugas', function () {
-    return view('welcome');
-});
-// // Route::get('/contact', [ContactForm::class,'view']);
-// Route::post('/contact-form', [ContactForm::class,'contactFormSubmit']);
-Route::get('/', [UserController::class,'demo'])->name('landing');
-Route::get('/jadwal-1smp' ,[JadwalMapelController::class,'jadwal1SMP'])->name('jadwal1smp');
-Route::get('/jadwal-2smp' ,[JadwalMapelController::class,'jadwal2SMP'])->name('jadwal2smp');
-Route::get('/jadwal-3smp' ,[JadwalMapelController::class,'jadwal3SMP'])->name('jadwal3smp');
-Route::get('/tambah-jadwal' ,[JadwalMapelController::class,'tambahJadwal']);
-Route::post('/buat' ,[JadwalMapelController::class,'buat']);
-// Route::get('/hapus-jadwal' ,[JadwalMapelController::class,'hapusJadwal']);
-// Route::delete('/hapus' ,[JadwalMapelController::class,'hapus']);
-Route::get('/alpha-1smp' ,[AlphaController::class,'alpha1SMP']);
-Route::get('/alpha-2smp' ,[AlphaController::class,'alpha2SMP']);
-Route::get('/alpha-3smp' ,[AlphaController::class,'alpha3SMP']);
-Route::get('/tambahAlpha', [AlphaController::class, 'tambahAlpha']);
-Route::post('/tambah', [AlphaController::class, 'tambah']);
-Route::get('potongan-gaji', [GuruController::class,'potonganGaji'])->name('potongan');
-Route::get('tambah-potongan',[GuruController::class,'tambahpotongan']);
-Route::post('/potong',[GuruController::class,'potong']);
-Route::get('tambah/jadwal', [JadwalMapelController::class,'tambahJadwal']);
-Route::get('tambah/alpha', [AlphaController::class,'tambahAlpha']);
-Route::get('/peraturan', [JadwalMapelController::class,'peraturan']);
-Route::get('/peraturan/cetak_pdf', [JadwalMapelController::class,'cetak_pdf']);
-Route::get('/profile-1smp', [UserController::class,'profile1SMP']);
-Route::get('/profile-2smp', [UserController::class,'profile2SMP']);
-Route::get('/profile/{id}', [UserController::class,'profile']);
-Route::get('/profile-3smp', [UserController::class,'profile3SMP']);
+    return view('welcome')->middleware('auth');
+})->middleware('auth');
+// // Route::get('/contact', [ContactForm::class,'view'])->middleware('auth');
+// Route::post('/contact-form', [ContactForm::class,'contactFormSubmit'])->middleware('auth');
+Route::get('/', [UserController::class,'demo'])->name('landing')->middleware('auth');
+Route::get('/jadwal-1smp' ,[JadwalMapelController::class,'jadwal1SMP'])->name('jadwal1smp')->middleware('auth');
+Route::get('/jadwal-2smp' ,[JadwalMapelController::class,'jadwal2SMP'])->name('jadwal2smp')->middleware('auth');
+Route::get('/jadwal-3smp' ,[JadwalMapelController::class,'jadwal3SMP'])->name('jadwal3smp')->middleware('auth');
+Route::get('/tambah-jadwal' ,[JadwalMapelController::class,'tambahJadwal'])->middleware('auth');
+Route::post('/buat' ,[JadwalMapelController::class,'buat'])->middleware('auth');
+// Route::get('/hapus-jadwal' ,[JadwalMapelController::class,'hapusJadwal'])->middleware('auth');
+// Route::delete('/hapus' ,[JadwalMapelController::class,'hapus'])->middleware('auth');
+Route::get('/alpha-1smp' ,[AlphaController::class,'alpha1SMP'])->middleware('auth');
+Route::get('/alpha-2smp' ,[AlphaController::class,'alpha2SMP'])->middleware('auth');
+Route::get('/alpha-3smp' ,[AlphaController::class,'alpha3SMP'])->middleware('auth');
+Route::get('/tambahAlpha', [AlphaController::class, 'tambahAlpha'])->middleware('auth');
+Route::post('/tambah', [AlphaController::class, 'tambah'])->middleware('auth');
+Route::get('potongan-gaji', [GuruController::class,'potonganGaji'])->name('potongan')->middleware('auth');
+Route::get('tambah-potongan',[GuruController::class,'tambahpotongan'])->middleware('auth');
+Route::post('/potong',[GuruController::class,'potong'])->middleware('auth');
+Route::get('tambah/jadwal', [JadwalMapelController::class,'tambahJadwal'])->middleware('auth');
+Route::get('tambah/alpha', [AlphaController::class,'tambahAlpha'])->middleware('auth');
+Route::get('/peraturan', [JadwalMapelController::class,'peraturan'])->middleware('auth');
+Route::get('/peraturan/cetak_pdf', [JadwalMapelController::class,'cetak_pdf'])->middleware('auth');
+Route::get('/profile-1smp', [UserController::class,'profile1SMP'])->middleware('auth');
+Route::get('/profile-2smp', [UserController::class,'profile2SMP'])->middleware('auth');
+Route::get('/profile/{id}', [UserController::class,'profile'])->middleware('auth');
+Route::get('/profile-3smp', [UserController::class,'profile3SMP'])->middleware('auth');
 // https://youtu.be/L7EGD2V_Zj4 
-Auth::routes();
+Auth::routes()->middleware('auth');
 
-Route::get('/profile-pribadi/{name}', [UserController::class,'profilePribadi'])->name('profile');
-Route::get('/edit-profile/{name}',[UserController::class,'editProfile']);
-Route::put('/edit/{name}',[UserController::class,'edit']);
-Route::get('/tambah-user',[UserController::class,'tambahUser']);
-Route::post('/tambah',[UserController::class,'tambah']);
+Route::get('/profile-pribadi/{name}', [UserController::class,'profilePribadi'])->name('profile')->middleware('auth');
+Route::get('/edit-profile/{name}',[UserController::class,'editProfile'])->middleware('auth');
+Route::put('/edit/{name}',[UserController::class,'edit'])->middleware('auth');
+Route::get('/tambah-user',[UserController::class,'tambahUser'])->middleware('auth');
+Route::post('/tambah',[UserController::class,'tambah'])->middleware('auth');
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/evaluasi', [App\Http\Controllers\GuruController::class, 'evaluasi'])->name('evaluasi');
-Route::get('/pesan-1smp', [App\Http\Controllers\GuruController::class, 'terima1SMP']);
-Route::get('/pesan-2smp', [App\Http\Controllers\GuruController::class, 'terima2SMP']);
-Route::get('/pesan-3smp', [App\Http\Controllers\GuruController::class, 'terima3SMP']);
-Route::get('/lihat/{id}', [App\Http\Controllers\GuruController::class, 'lihat']);
-Route::get('/home', [ChartJsController::class, 'index']);
+Route::get('/evaluasi', [App\Http\Controllers\GuruController::class, 'evaluasi'])->name('evaluasi')->middleware('auth');
+Route::get('/pesan-1smp', [App\Http\Controllers\GuruController::class, 'terima1SMP'])->middleware('auth');
+Route::get('/pesan-2smp', [App\Http\Controllers\GuruController::class, 'terima2SMP'])->middleware('auth');
+Route::get('/pesan-3smp', [App\Http\Controllers\GuruController::class, 'terima3SMP'])->middleware('auth');
+Route::get('/lihat/{id}', [App\Http\Controllers\GuruController::class, 'lihat'])->middleware('auth');
+Route::get('/home', [ChartJsController::class, 'index'])->middleware('auth');
