b127521d62b0e222384063fbf600420cbd03cd1b
Host Laravel <testinglaravel18@gmail.com>
2021-12-28 23:21:09 +0700

    commit-kbm

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -72,7 +72,7 @@
         <p class="alert alert-success alert-block">{{ $message }}</p>
     @enderror
     <div class="input-container ic2">
-      <input  wire:model="email" id="email" class="input" type="text"  name="email" value="{{ old('email') }}" placeholder="Masukkan Email" />
+      <input hidden wire:model="email" id="email" class="input" type="text"  name="email" value="value="{{ Auth::user()->email }}" placeholder="Masukkan Email" />
       {{-- <div class="cut cut-short"></div>
       <label for="email" class="placeholder">Email</> --}}
     </div>
