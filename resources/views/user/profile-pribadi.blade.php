da845ec935ace0235bcebdbfd0b8aed71e2cc66a
Host Laravel <testinglaravel18@gmail.com>
2021-12-30 20:55:18 +0700

    commit-kbm

--- resources/views/user/profile-pribadi.blade.php
+++ resources/views/user/profile-pribadi.blade.php
@@ -11,7 +11,7 @@
                         <strong>{{ $message }}</strong>
                     </div>
                 @endif
-                <div class="card user-card-full" style="width: 300px">
+                <div class="card user-card-full" style="width: 250px">
                     <div class="row m-l-0 m-r-0">
                         
                         <div class="col-sm-4 bg-c-lite-green user-profile">
