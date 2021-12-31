9c90dfa3b8d736fd9871f4aba9ca382cd4705dd0
Host Laravel <testinglaravel18@gmail.com>
2021-12-31 16:14:09 +0700

    commit-kbm

--- resources/views/user/profile-pribadi.blade.php
+++ resources/views/user/profile-pribadi.blade.php
@@ -17,12 +17,12 @@
                         <div class="col-sm-4 bg-c-lite-green user-profile">
                             <div class="card-block text-center text-wprofile.htmlhite">
                                 <div class="m-b-25"> 
-                                    {{-- <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image">  --}}
-                                    @if (Auth::user()->name  == '')
+                                    <img src="https://drive.google.com/file/d/1OXqqex5dvdIvO2bbV5RCploV4xfJ3tJu/view?usp=sharing" class="img-radius" alt="User-Profile-Image"> 
+                                    {{-- @if (Auth::user()->name  == '')
                                     <img src="https://ui-avatars.com/api/?name={{ Auth::user()->username }}" alt="..."  class=" rounded-circle foto-profile" rounded-circle alt="User-Profile-Image">
                                     @elseif (Auth::user()->name  != '')
                                     <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" alt="..."  class=" rounded-circle foto-profile" rounded-circle alt="User-Profile-Image">
-                                    @endif
+                                    @endif --}}
                                 </div>
                                 <h6 class="f-w-600">{{ Auth::user()->name }}</h6>
                                 
