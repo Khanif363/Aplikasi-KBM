839823956734d34354b70d5ea9dbe9862bb52b2e
Host Laravel <testinglaravel18@gmail.com>
2022-01-01 22:48:35 +0700

    commit-kbm

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -70,11 +70,11 @@ class DatabaseSeeder extends Seeder
 
         Peraturan::create([
             'id'            => '1',
-            'peraturan'     => '1. Memberikan sambutan salam kepada guru yang baru memasuki kelas.<br>
-            2. Selalu menggunakan seragam dengan benar dan rapi.<br>
-            3. Mendengarkan dengan baik apa yang guru sampaikan.<br>
-            3. Dilarang meninggalkan kelas sebelum diizinkan oleh guru.<br>
-            4. Dilarang merokok,pacaran dan hal-hal sebagainya yang dilarang dalam islam.<br>
+            'peraturan'     => '1. Memberikan sambutan salam kepada guru yang baru memasuki kelas.
+            2. Selalu menggunakan seragam dengan benar dan rapi.
+            3. Mendengarkan dengan baik apa yang guru sampaikan.
+            3. Dilarang meninggalkan kelas sebelum diizinkan oleh guru.
+            4. Dilarang merokok,pacaran dan hal-hal sebagainya yang dilarang dalam islam.
             5. Mengerjakan tugas yang diberikan oleh guru tepat pada waktunya.'
         ]);
 
