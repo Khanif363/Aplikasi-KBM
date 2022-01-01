5cc457da8b6d2bdd4197ef36fe47c3b15dab7d49
Host Laravel <testinglaravel18@gmail.com>
2022-01-01 14:33:17 +0700

    commit-kbm

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,7 +69,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 
 <div class="limiter">
@@ -82,7 +82,7 @@
                 @csrf
 
                 <div class="wrap-input100 validate-input" data-validate = "Enter Email">
-                    <input class="input100" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <input class="input100" placeholder="Masukkan Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
@@ -92,7 +92,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input" data-validate="Enter password">
-                    <input class="input100" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
+                    <input class="input100" placeholder="Masukkan Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
                     <span class="focus-input100" data-placeholder="&#xe80f;"></span>
                 </div>
 
