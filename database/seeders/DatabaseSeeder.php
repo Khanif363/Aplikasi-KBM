1aa6e26e1e48df89180070a2d6b7cc770d12621a
Host Laravel <testinglaravel18@gmail.com>
2022-01-01 21:59:48 +0700

    commit-kbm

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Kelas;
 use App\Models\Waktu;
 use App\Models\Tanggal;
+use App\Models\Peraturan;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -63,9 +64,19 @@ class DatabaseSeeder extends Seeder
         // ]);
         // DB::table('jadwals')->delete();
 
-        Schema::table('jadwals', function(\Illuminate\Database\Schema\Blueprint $table) {
-            $table->foreignId('user_id')->after('id');
-        });
+        // Schema::table('jadwals', function(\Illuminate\Database\Schema\Blueprint $table) {
+        //     $table->foreignId('user_id')->after('id');
+        // });
+
+        Peraturan::create([
+            'id'            => '1',
+            'peraturan'     => '1. Memberikan sambutan salam kepada guru yang baru memasuki kelas.<br>
+            2. Selalu menggunakan seragam dengan benar dan rapi.<br>
+            3. Mendengarkan dengan baik apa yang guru sampaikan.<br>
+            3. Dilarang meninggalkan kelas sebelum diizinkan oleh guru.<br>
+            4. Dilarang merokok,pacaran dan hal-hal sebagainya yang dilarang dalam islam.<br>
+            5. Mengerjakan tugas yang diberikan oleh guru tepat pada waktunya.'
+        ]);
 
 
     }
