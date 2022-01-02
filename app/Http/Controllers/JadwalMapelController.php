a08e3ef8220e3b2ec2e870ec999efef010edeb83
Host Laravel <testinglaravel18@gmail.com>
2022-01-02 21:19:51 +0700

    commit-kbm

--- app/Http/Controllers/JadwalMapelController.php
+++ app/Http/Controllers/JadwalMapelController.php
@@ -12,6 +12,7 @@ use App\Models\Jadwal;
 // use Barryvdh\DomPDF\Facade as PDF;
 use App\Models\Peraturan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JadwalMapelController extends Controller
 {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\User;
 use App\Models\Kelas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
