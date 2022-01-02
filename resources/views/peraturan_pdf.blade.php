56b26398c91a0f10e91f6668345ba0fb1c99c5d0
Host Laravel <testinglaravel18@gmail.com>
2022-01-03 06:35:35 +0700

    commit-kbm

--- resources/views/peraturan_pdf.blade.php
+++ resources/views/peraturan_pdf.blade.php
@@ -28,7 +28,7 @@
                 4. Dilarang merokok,pacaran dan hal-hal sebagainya yang dilarang dalam islam.<br>
                 5. Mengerjakan tugas yang diberikan oleh guru tepat pada waktunya.
             </p> --}}
-            {{ $peraturans->peraturan }}
+            {!! $peraturans->peraturan !!}
             @endforeach
         </div>
     </div>  
