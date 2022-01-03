839e53e16e7b15b63e406a4f6eb2a25fe8065a5c
Host Laravel <testinglaravel18@gmail.com>
2022-01-03 20:28:48 +0700

    commit-kbm

--- resources/views/user/profile-pribadi.blade.php
+++ resources/views/user/profile-pribadi.blade.php
@@ -61,7 +61,7 @@
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <p class="m-b-10 f-w-600">Tanggal</p>
-                                        <h6 class="text-muted f-w-400">{{ Auth::user()->tanggal_lahir->format('dddd, D MMMM Y')}}</h6>
+                                        <h6 class="text-muted f-w-400">{{ Auth::user()->tanggal_lahir->translatedFormat('l, d F Y')}}</h6>
                                         {{-- format('dddd, D MMMM Y')}} --}}
                                     </div>
                                 </div>
